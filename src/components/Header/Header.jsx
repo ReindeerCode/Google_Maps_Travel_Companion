1e0bc0939a67b899ba6c0727d2912f6f84a7a6b4
MJRudolph <ReindeerCode@gmail.com>
2022-01-25 18:01:43 -0500

    added imports and started body of Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+
 
 const Header = () => {
     return(
-        <h1>Header</h1>
+        <AppBar position="static">
+            <Toolbar className={classes.Toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex">
+                <Typography variant="h6" className={classes.title}>
+                    Explore New Places
+                </Typography>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
