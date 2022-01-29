f6464f3cfdf846d4ca005941438aa42907e5b454
MJRudolph <ReindeerCode@gmail.com>
2022-01-29 15:21:00 -0500

    fixed type on Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     const classes = useStyles();
     return(
         <AppBar position="static">
-            <Toolbar className={classes.Toolbar}>
+            <Toolbar className={classes.toolbar}>
                 <Typography variant="h5" className={classes.title}>
                     Travel Advisor
                 </Typography>
