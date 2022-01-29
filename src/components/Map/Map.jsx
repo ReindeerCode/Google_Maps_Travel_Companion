325be88421f4113ecc820fb00a7b5156f43bd881
MJRudolph <ReindeerCode@gmail.com>
2022-01-29 15:32:29 -0500

    started Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-
-
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab'
 import useStyles from './styles'
 
 const Map = () => {
