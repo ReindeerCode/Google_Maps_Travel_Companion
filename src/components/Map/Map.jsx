05b1019a54e45aaa5da9f35189f97e362f67d535
MJRudolph <ReindeerCode@gmail.com>
2022-02-07 18:29:04 -0500

    starting location stuff

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,33 +1,31 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
-import useStyles from './styles';
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+import useStyles from "./styles";
 
-const Map = () => {
-    const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width:600px)');
-    
-    const coordinates = { lat: 0, lng: 0 };
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
 
-    return(
-        <div className={classes.mapContainer}>
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyBjSxJeXwY2lChvrDwg4XRg_ZyWMJ2hWx0' }}
-                defaultCenter={coordinates}
-                center={coordinates}
-                defaultZoom={14}
-                margin={[50, 50,50,50]}
-                options={''}
-                onChange={''}
-                onChildClick={''}
-            >
-
-            </GoogleMapReact>
-
-        </div>
-    );
-}
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBjSxJeXwY2lChvrDwg4XRg_ZyWMJ2hWx0" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+        }}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
 export default Map;
